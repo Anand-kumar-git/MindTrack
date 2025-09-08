04f614c4188d8bc96a57c8fe8414a37cead0edf3
Anand-kumar-git <danandkumar2003@gmail.com>
2025-09-08 12:21:40 +0530

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,7 +4,7 @@ set -e
 echo "Starting new container..."
 
 # Login to ECR
-aws ecr get-login-password --region <your-region> | docker login --username AWS --password-stdin <your-account-id>.dkr.ecr.<your-region>.amazonaws.com
+aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 787755074992.dkr.ecr.ap-south-1.amazonaws.com
 
 # Extract image URI from imagedefinitions.json
 IMAGE_URI=$(cat /home/ubuntu/app/imagedefinitions.json | jq -r '.[0].imageUri')
